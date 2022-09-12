95c6874cbc8ee2dc22286c3eab9a7830d31eacbc
kapilesh <kapilesh1@gmail.com>
2022-09-12 08:28:51 +0530

    backend setUp

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-
-app.listen(5000,()=>{console.log("server is running successfully")})
+const port = 3000
+app.listen(port,()=>{console.log(`server is running successfully at ${port}`)})
