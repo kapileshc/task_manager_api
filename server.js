c1e628543e8ad66f4a927746134e9f50e423ecd0
kapilesh <kapilesh1@gmail.com>
2022-09-12 23:23:40 +0530

    routes and controller setup

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/task')
 const port = 3000
+
+app.use('/api/v1/tasks',tasks)
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
 app.listen(port,()=>{console.log(`server is running successfully at ${port}`)})
