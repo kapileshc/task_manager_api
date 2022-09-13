147c82f1c2f8f5dc1a0938053a0acfd31c169714
kapilesh <kapilesh1@gmail.com>
2022-09-14 01:21:39 +0530

    basic validation in the mongoose schema

--- model/task.js
+++ model/task.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 const TaskSchema = new mongoose.Schema({
-    name:String,completed:Boolean
+    name:{type:String,required:[true,'name is required'],trim:true,maxlength:[15,'name cannot be more than 15 letter']},
+    completed:{type:Boolean,default:false,}
 })
 
 module.exports = mongoose.model('Task',TaskSchema)
