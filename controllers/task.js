d52195c516974859f626eb283a549e0751bd1809
kapilesh <kapilesh1@gmail.com>
2022-09-13 19:34:10 +0530

    mongoDB connection

--- controllers/task.js
+++ controllers/task.js
@@ -2,7 +2,24 @@ const getAllTasks = (req,res) =>{
     res.send('all items')
 }
 
+const createTask = (req,res) =>{
+    res.send('hello create')
+    console.log({...req.body})
+    res.json(req.body)
+}
+
+const getTask = (req,res) =>{
+    res.send(req.params.id)
+}
+
+const updateTask = (req,res) =>{
+    res.send('update task')
+}
+
+const deleteTask = (req,res) =>{
+    res.send('delete task')
+}
 
 
 
-module.exports = {getAllTasks}
+module.exports = {getAllTasks,getTask,createTask,updateTask,deleteTask}
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+require('./db/connection')
 const express = require('express')
 const app = express()
 const tasks = require('./routes/task')
+const connectDB = require('./db/connection')
 const port = 3000
+require('dotenv').config()
 
+app.use(express.json())
 app.use('/api/v1/tasks',tasks)
 
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
-app.listen(port,()=>{console.log(`server is running successfully at ${port}`)})
+
+const start = async() =>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`Server is listening to the port ${port}`)) 
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
